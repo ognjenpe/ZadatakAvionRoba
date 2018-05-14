3484096dd3ffe53257c67d02db4e10e142ddce17
ognjenpe <ognjenpejovic@gmail.com>
2018-05-14 19:30:03 +0200

    Uradjene klase Avion i Roba closes #3

--- src/main/java/model/Avion.java
+++ src/main/java/model/Avion.java
@@ -1,4 +1,51 @@
 package model;
 
+import com.j256.ormlite.field.DatabaseField;
+
 public class Avion {
+
+
+
+    public static final String POLJE_OZNAKA = "oznaka";
+    public static final String POLJE_RASPONKRILA = "raspon_krila";
+
+    @DatabaseField(generatedId = true)
+    private int id;
+    @DatabaseField(columnName = POLJE_OZNAKA, canBeNull = false, unique = false)
+    private String oznaka;
+    @DatabaseField(columnName = POLJE_RASPONKRILA)
+    private int rasponKrila;
+
+    public Avion(){
+
+    }
+    public Avion(String oznaka, int raspon_krila){
+        this.oznaka = oznaka;
+        this.rasponKrila = raspon_krila;
+    }
+
+    public String getOznaka() {
+        return oznaka;
+    }
+
+    public void setOznaka(String oznaka) {
+        this.oznaka = oznaka;
+    }
+
+    public int getRasponKrila() {
+        return rasponKrila;
+    }
+
+    public void setRasponKrila(int rasponKrila) {
+        this.rasponKrila = rasponKrila;
+    }
+
+    @Override
+    public String toString() {
+        return "Avion{" +
+                "id=" + id +
+                ", oznaka='" + oznaka + '\'' +
+                ", rasponKrila=" + rasponKrila +
+                '}';
+    }
 }
--- src/main/java/model/Roba.java
+++ src/main/java/model/Roba.java
@@ -1,4 +1,66 @@
 package model;
 
+import com.j256.ormlite.field.DatabaseField;
+
 public class Roba {
+
+
+
+    public static final String POLJE_NAZIV = "naziv";
+    public static final String POLJE_OPIS = "opis";
+    public static final String POLJE_TEZINA = "tezina";
+
+    @DatabaseField(generatedId = true)
+    private int id;
+    @DatabaseField(columnName = POLJE_NAZIV, canBeNull = false, unique = false)
+    private String naziv;
+    @DatabaseField(columnName = POLJE_OPIS)
+    private String opis;
+    @DatabaseField(columnName = POLJE_TEZINA)
+    private double tezina;
+
+    public Roba(){
+
+    }
+
+    public Roba(String naziv, String opis, double tezina){
+        this.naziv = naziv;
+        this.opis = opis;
+        this.tezina = tezina;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public String getOpis() {
+        return opis;
+    }
+
+    public void setOpis(String opis) {
+        this.opis = opis;
+    }
+
+    public double getTezina() {
+        return tezina;
+    }
+
+    public void setTezina(double tezina) {
+        this.tezina = tezina;
+    }
+
+    @Override
+    public String toString() {
+        return "Roba{" +
+                "id=" + id +
+                ", naziv='" + naziv + '\'' +
+                ", opis='" + opis + '\'' +
+                ", tezina=" + tezina +
+                '}';
+    }
+    
 }
